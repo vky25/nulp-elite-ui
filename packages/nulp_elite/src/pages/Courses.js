e87ca1e20f91db2c6bd0351102f0b036c2402dec
paritshivani <shivani_p@techjoomla.com>
2024-02-12 15:51:53 +0530

    structure and menu

---
+++ packages/nulp_elite/src/pages/Courses.js
@@ -0,0 +1,21 @@
+// Courses.js
+import React from 'react';
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+
+const Courses = () => {
+  return (
+    <Box textAlign="center" padding="10">
+      <Heading as="h1" size="2xl" marginBottom="4">
+        Welcome to Our Learning Portal
+      </Heading>
+      <Text fontSize="xl" marginBottom="8">
+        Enhance your knowledge and skills with our diverse range of courses and content.
+      </Text>
+      <Button colorScheme="blue" size="lg">
+        Explore Courses
+      </Button>
+    </Box>
+  );
+};
+
+export default Courses;
