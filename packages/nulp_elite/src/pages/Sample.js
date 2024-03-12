1d43fa4a6ba025e6de17ed5ddc361fab687bec61
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-03-12 12:13:38 +0530

    change search and notification bell icon size

--- packages/nulp_elite/src/pages/Sample.js
+++ packages/nulp_elite/src/pages/Sample.js
@@ -16,7 +16,7 @@ import { useTranslation } from "react-i18next";
 import moment from "moment";
 import manifest from "../../src/manifest.json";
 import "ag-grid-community/styles/ag-grid.css";
-import { ChakraProvider, CSSReset, Box, Container, Button } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset, Container } from '@chakra-ui/react';
 import FilterPopup from './../components/FilterPopup';
 
 export default function Sample() {
