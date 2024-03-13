d5eac9f34f11686583860781a3e1c79238bb4094
paritshivani <shivani_p@techjoomla.com>
2024-03-13 17:53:43 +0530

    repo cleanup

---
+++ module-template/craco.config.js
@@ -0,0 +1,16 @@
+const cracoModuleFederation = require("craco-module-federation");
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+
+module.exports = {
+  devServer: {
+    port: 3300,
+  },
+  webpack: {
+    plugins: [new ExternalTemplateRemotesPlugin()],
+  },
+  plugins: [
+    {
+      plugin: cracoModuleFederation,
+    },
+  ],
+};
