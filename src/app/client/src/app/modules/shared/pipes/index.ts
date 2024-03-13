7863651004fd2777726a07ea0526cce00d9baea2
Shivani Parit <58217306+paritshivani@users.noreply.github.com>
2024-03-13 16:58:32 +0530

    Delete src directory

--- src/app/client/src/app/modules/shared/pipes/index.ts
+++
@@ -1,7 +0,0 @@
-export *  from './date-format/date-format.pipe';
-export * from './filter/filter.pipe';
-export * from './filter/filter.pipe';
-export * from './interpolate/interpolate.pipe';
-export * from './sortBy/sortBy.pipe';
-export * from './sb-data-table-pipe/sb-data-table.pipe';
-export * from './participants.pipe';
