126d9ee668e7a03b9da4aa9a8fcb3141882e7570
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-04-05 21:48:05 +0530

    chnge in search component

--- packages/nulp_elite/src/components/search.js
+++ packages/nulp_elite/src/components/search.js
@@ -1,109 +1,43 @@
 import React, { useState } from "react";
-import { Button } from "@mui/material";
+import { Button } from "@mui/base/Button";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
-import TextField from "@mui/material/TextField";
 
-export default function Search() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [userData, setUserData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+function Search({ onSearch }) {
+  const [query, setQuery] = useState("");
 
-  const handleSearch = async () => {
-    setIsLoading(true);
-    setError(null);
-    setUserData([]);
-
-    const url = `http://localhost:3000/learner/user/v3/search`;
-    const requestBody = {
-      request: {
-        filters: {
-          status: "1",
-          rootOrgId: "0130701891041689600",
-        },
-        query: searchQuery,
-      },
-    };
-
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestBody),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to search data");
-      }
-
-      const responseData = await response.json();
-      setUserData(responseData.result.response.content);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearchClick = () => {
+    onSearch(query);
   };
-
-  const filteredUsers = userData?.filter(
-    (user) => user.name && user.name.includes(searchQuery)
-  );
-
-  return (
-    <Box
+  <Box style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}>
+    <input
+      type="text"
+      // value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder="Search..."
       style={{
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column", // Added to align items vertically
+        width: "100%",
+        flex: 1,
+        marginRight: "0.5rem",
+        padding: "0.5rem",
+        borderRadius: "4px",
+        border: "1px solid #CACACA",
+      }}
+    />
+    <Button
+      onClick={handleSearchClick}
+      style={{
+        padding: "11px 9px",
+        borderRadius: "4px",
+        backgroundColor: "#004367",
+        color: "white",
+        border: "1px",
+        cursor: "pointer",
+        fontSize: "12px",
       }}
     >
-      <TextField
-        label="Search for a user..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        style={{
-          width: "100%",
-          marginBottom: "1rem",
-        }}
-      />
-      <Button
-        style={{
-          padding: "11px 9px",
-          borderRadius: "4px",
-          backgroundColor: "#004367",
-          color: "white",
-          border: "1px",
-          cursor: "pointer",
-          fontSize: "12px",
-        }}
-        onClick={handleSearch}
-      >
-        Search
-      </Button>
-      {!isLoading && !error && (
-        <List>
-          {filteredUsers.map((user, index) => (
-            <React.Fragment key={index}>
-              <ListItem>
-                <ListItemText
-                  primary={`Name Surname: ${user.firstName} ${user.lastName}`}
-                  secondary={`Designation: ${user.designation}`}
-                />
-              </ListItem>
-              <Divider />
-            </React.Fragment>
-          ))}
-        </List>
-      )}
-      {isLoading && <Typography>Loading...</Typography>}
-      {error && <Typography>Error: {error}</Typography>}
-    </Box>
-  );
+      Search
+    </Button>
+  </Box>;
 }
+
+export default Search;
