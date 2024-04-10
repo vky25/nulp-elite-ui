d00de88afdb2d593c38204a71a8ec3e025387280
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-04-10 14:21:39 +0530

    change in app.js

--- packages/nulp_elite/src/App.js
+++ packages/nulp_elite/src/App.js
@@ -34,7 +34,6 @@ import Certificate from "pages/profile/certificate";
 
 import FAQPage from "pages/FAQPage";
 import AddConnections from "pages/connections/AddConnections";
-import MyConnections from "pages/connections/MyConnections";
 import DomainList from "pages/search/DomainList";
 import Registrationnew from "pages/registration/Registration";
 import Registrationold from "pages/registration/Registrationold";
@@ -101,11 +100,6 @@ function App() {
       moduleName: "nulp_elite",
       path: "/addConnections",
       component: AddConnections,
-    },
-    {
-      moduleName: "nulp_elite",
-      path: "/connections/myConnections",
-      component: MyConnections,
     },
     {
       moduleName: "nulp_elite",
