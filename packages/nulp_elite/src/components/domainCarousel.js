8be040d246fb75a76be552ce5c113187efd867cd
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-05-22 13:30:46 +0530

    show 10 domains on both screen

--- packages/nulp_elite/src/components/domainCarousel.js
+++ packages/nulp_elite/src/components/domainCarousel.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import Box from '@mui/joy/Box';
-import Typography from '@mui/joy/Typography';
+import React, { useState, useEffect } from "react";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import domainWithImage from "../assets/domainImgForm.json";
@@ -9,32 +9,30 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5
+    items: 5,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 8
+    items: 8,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 2,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 4
-  }
+    items: 4,
+  },
 };
 
-
-export default function DomainCarousel({ domains ,onSelectDomain}) {
-
+export default function DomainCarousel({ domains, onSelectDomain }) {
   const dotsToShow = 4; // Number of dots to display
   const [isActive, setIsActive] = useState(false);
   const [itemsArray, setItemsArray] = useState([]);
   const [data, setData] = React.useState();
-  const [activeStates, setActiveStates] = useState(() => (
-    domains?.map(() => false) // Initialize all items as inactive
-  ));
+  const [activeStates, setActiveStates] = useState(
+    () => domains?.map(() => false) // Initialize all items as inactive
+  );
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
   const [activeDomain, setActiveDomain] = useState(null);
   const [scrolled, setScrolled] = useState(false);
@@ -51,9 +49,9 @@ export default function DomainCarousel({ domains ,onSelectDomain}) {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   useEffect(() => {
@@ -70,15 +68,15 @@ export default function DomainCarousel({ domains ,onSelectDomain}) {
     });
     const croppedArray = itemsArray?.slice(0, 10);
     setData(croppedArray);
-    
-    console.log("itemsArray---",itemsArray)
-    console.log("data---",data)
+
+    console.log("itemsArray---", itemsArray);
+    console.log("data---", data);
   }, []);
-    // Function to push data to the array
-    const pushData = (term) => {
-      setItemsArray((prevData) => [...prevData, term]);
-    };
-  const handleDomainClick = (query,index) => {
+  // Function to push data to the array
+  const pushData = (term) => {
+    setItemsArray((prevData) => [...prevData, term]);
+  };
+  const handleDomainClick = (query, index) => {
     const newActiveStates = [...activeStates]; // Create a copy of activeStates
     newActiveStates[index] = !newActiveStates[index]; // Toggle the active state at the clicked index
     setActiveStates(newActiveStates);
@@ -93,77 +91,125 @@ export default function DomainCarousel({ domains ,onSelectDomain}) {
     setActiveDomain(null);
   };
 
- 
   return (
-   
-   <Box style={{position:'relative'}} className="bg-blue">
-   
-   {isMobile ? (
-    <>
-          <Carousel swipeable={false}
-          draggable={false}
-          showDots={["mobile"]} // Show dots only if there are more than 4 items
-          responsive={responsive}
-          ssr={true} // means to render carousel on server-side.
-          infinite={true}
-          autoPlaySpeed={1000}
-          keyBoardControl={true}
-          customTransition="all .5"
-          transitionDuration={500}
-          containerClass="carousel-container"
-          dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-40-px">
-                  {itemsArray && itemsArray?.map((domain, index) => (
-
-          <Box  className={`my-class ${activeStates[index] ? 'carousel-active-ui' : ''}`} onClick={(e) => handleDomainClick(domain.code,index)}  key={index} orientation="horizontal" size="sm" variant="outlined" style={{display:'flex'}}>
-              <Box className="imgBorder" style={{background:'#fff',padding:'10px',borderRadius:'10px',height:'45px',width:'45px'}}>
-              {/* {(domain.image != undefined) && <img src={require(baseImgUrl+domain.image)}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} />}
+    <Box style={{ position: "relative" }} className="bg-blue">
+      {isMobile ? (
+        <>
+          <Carousel
+            swipeable={false}
+            draggable={false}
+            showDots={["mobile"]} // Show dots only if there are more than 4 items
+            responsive={responsive}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition="all .5"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
+            {itemsArray &&
+              itemsArray?.slice(0, 10).map((domain, index) => (
+                <Box
+                  className={`my-class ${
+                    activeStates[index] ? "carousel-active-ui" : ""
+                  }`}
+                  onClick={(e) => handleDomainClick(domain.code, index)}
+                  key={index}
+                  orientation="horizontal"
+                  size="sm"
+                  variant="outlined"
+                  style={{ display: "flex" }}
+                >
+                  <Box
+                    className="imgBorder"
+                    style={{
+                      background: "#fff",
+                      padding: "10px",
+                      borderRadius: "10px",
+                      height: "45px",
+                      width: "45px",
+                    }}
+                  >
+                    {/* {(domain.image != undefined) && <img src={require(baseImgUrl+domain.image)}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} />}
                 {(domain.image == undefined)&& <img src={require("../assets/swm.png")}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} />} */}
-                <img
-                        src={require(`../assets/domainImgs${domain.image}`)}
-                        style={{width:'40px',objectFit:'contain'}} alt={domain.name}
-                      />
-                {/* <img src={require("../assets/swm.png")}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} /> */}
+                    <img
+                      src={require(`../assets/domainImgs${domain.image}`)}
+                      style={{ width: "40px", objectFit: "contain" }}
+                      alt={domain.name}
+                    />
+                    {/* <img src={require("../assets/swm.png")}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} /> */}
+                  </Box>
+                  <Box
+                    sx={{ alignSelf: "center", padding: "0 19px 0 5px" }}
+                    className="xs-hide"
+                  >
+                    <Typography level="title-md" style={{ fontSize: "12px" }}>
+                      {domain.name}
+                    </Typography>
+                  </Box>
                 </Box>
-            <Box sx={{ alignSelf:'center',padding:'0 19px 0 5px' }} className="xs-hide">
-              <Typography level="title-md" style={{fontSize:'12px'}}>{domain.name}</Typography>
-            </Box>
-          </Box>
-                 ))}
-
+              ))}
           </Carousel>
-            <Box className="leftshade"></Box>
-            <Box className="rightshade"></Box>
-            </>
-          ) : (
-            <Box sx={{display:"flex"}}  className={scrolled ? "bg-blue scrolled" : "bg-blue"}>
-                                {itemsArray && itemsArray?.map((domain, index) => (
-
-               <Box  className={`my-class ${activeStates[index] ? 'carousel-active-ui' : ''}`} onClick={(e) => handleDomainClick(domain.code,index)}  key={index} orientation="horizontal" size="sm" variant="outlined" style={{display:'flex',margin:"0 4px"}}>
-              <Box className="imgBorder" style={{background:'#fff',padding:'10px',borderRadius:'10px',height:'45px',width:'45px'}}>
-              {/* {(domain.image != undefined) && <img src={require(baseImgUrl+domain.image)}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} />}
+          <Box className="leftshade"></Box>
+          <Box className="rightshade"></Box>
+        </>
+      ) : (
+        <Box
+          sx={{ display: "flex" }}
+          className={scrolled ? "bg-blue scrolled" : "bg-blue"}
+        >
+          {itemsArray &&
+            itemsArray?.slice(0, 10).map((domain, index) => (
+              <Box
+                className={`my-class ${
+                  activeStates[index] ? "carousel-active-ui" : ""
+                }`}
+                onClick={(e) => handleDomainClick(domain.code, index)}
+                key={index}
+                orientation="horizontal"
+                size="sm"
+                variant="outlined"
+                style={{ display: "flex", margin: "0 4px" }}
+              >
+                <Box
+                  className="imgBorder"
+                  style={{
+                    background: "#fff",
+                    padding: "10px",
+                    borderRadius: "10px",
+                    height: "45px",
+                    width: "45px",
+                  }}
+                >
+                  {/* {(domain.image != undefined) && <img src={require(baseImgUrl+domain.image)}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} />}
                 {(domain.image == undefined)&& <img src={require("../assets/swm.png")}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} />} */}
-                <img
-                        src={require(`../assets/domainImgs${domain.image}`)}
-                        style={{width:'40px',objectFit:'contain'}} alt={domain.name}
-                      />
-                {/* <img src={require("../assets/swm.png")}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} /> */}
+                  <img
+                    src={require(`../assets/domainImgs${domain.image}`)}
+                    style={{ width: "40px", objectFit: "contain" }}
+                    alt={domain.name}
+                  />
+                  {/* <img src={require("../assets/swm.png")}  style={{width:'40px',objectFit:'contain'}} alt={domain.name} /> */}
                 </Box>
                 {activeDomain === domain && (
-
-            <Box className="domainText" sx={{ alignSelf:'center',padding:'0 19px 0 5px' }}>
-              <Typography level="title-md" style={{fontSize:'12px',opacity: isActive ? 1 : 0,}}>{domain.name}</Typography>
-            </Box>
-                          )}
-
-          </Box>
-                           ))}
-
-            </Box>
-)}
-        
-
-      
+                  <Box
+                    className="domainText"
+                    sx={{ alignSelf: "center", padding: "0 19px 0 5px" }}
+                  >
+                    <Typography
+                      level="title-md"
+                      style={{ fontSize: "12px", opacity: isActive ? 1 : 0 }}
+                    >
+                      {domain.name}
+                    </Typography>
+                  </Box>
+                )}
+              </Box>
+            ))}
+        </Box>
+      )}
     </Box>
   );
 }
